d8fbad2f41ff6e1d529bb28e2cddd093988d552e
Mahmoud Gaber <71694567+MahmooudGaaber@users.noreply.github.com>
2025-07-17 12:10:44 +0300

    user & userTypes entity

---
+++ src/main/java/com/example/jobportal/entity/UsersType.java
@@ -0,0 +1,61 @@
+package com.example.jobportal.entity;
+
+import jakarta.persistence.*;
+import org.apache.catalina.User;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users_type")
+public class UsersType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userTypeId ;
+
+    public String userTypeName;
+
+    @OneToMany(targetEntity = Users.class , mappedBy = "userTypeId" , cascade = CascadeType.ALL)
+    private List<Users> users ;
+
+    public UsersType() {
+    }
+
+    public UsersType(int userTypeId, String userTypeName, List<Users> users) {
+        this.userTypeId = userTypeId;
+        this.userTypeName = userTypeName;
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "UsersType{" +
+                "userTypeId=" + userTypeId +
+                ", userTypeName='" + userTypeName + '\'' +
+                '}';
+    }
+
+    public int getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(int userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserTypeName() {
+        return userTypeName;
+    }
+
+    public void setUserTypeName(String userTypeName) {
+        this.userTypeName = userTypeName;
+    }
+
+    public List<Users> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Users> users) {
+        this.users = users;
+    }
+}
