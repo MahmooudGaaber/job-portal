56bf070f42c7ae4a8134a633576b67f50a7aefa0
Mahmoud Gaber <71694567+MahmooudGaaber@users.noreply.github.com>
2025-07-17 15:47:40 +0300

    register

--- src/main/java/com/example/jobportal/controller/UsersController.java
+++ src/main/java/com/example/jobportal/controller/UsersController.java
@@ -2,21 +2,26 @@ package com.example.jobportal.controller;
 
 import com.example.jobportal.entity.Users;
 import com.example.jobportal.entity.UsersType;
+import com.example.jobportal.services.UsersService;
 import com.example.jobportal.services.UsersTypesService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class UsersController {
     private final UsersTypesService usersTypesService;
+    private final UsersService usersService;
 
     @Autowired
-    public UsersController(UsersTypesService usersTypesService) {
+    public UsersController(UsersTypesService usersTypesService, UsersService usersService) {
         this.usersTypesService = usersTypesService;
+        this.usersService = usersService;
     }
 
     @GetMapping("/register")
@@ -25,6 +30,14 @@ public class UsersController {
         model.addAttribute("getAllTypes",usersTypes);
         model.addAttribute("user",new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users){
+        usersService.addNew(users);
+        System.out.println("User::"+users);
+        return "dashboard";
+
     }
 
 }
