84fea912d63fe670f8e2c0657362f2678343edc5
Mahmoud Gaber <71694567+MahmooudGaaber@users.noreply.github.com>
2025-07-18 11:29:08 +0300

    registration flow

--- src/main/java/com/example/jobportal/services/UsersService.java
+++ src/main/java/com/example/jobportal/services/UsersService.java
@@ -1,6 +1,10 @@
 package com.example.jobportal.services;
 
+import com.example.jobportal.entity.JobSeekerProfile;
+import com.example.jobportal.entity.RecruiterProfile;
 import com.example.jobportal.entity.Users;
+import com.example.jobportal.repository.JobSeekerProfileRepository;
+import com.example.jobportal.repository.RecruiterProfileRepository;
 import com.example.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +16,28 @@ import java.util.Optional;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final RecruiterProfileRepository recruiterProfileRepository;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
         this.usersRepository = usersRepository;
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.recruiterProfileRepository = recruiterProfileRepository;
     }
+
 
     public Users addNew(Users users){
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
-        return usersRepository.save(users);
+        int userTypeId = users.getUsersTypeId().getUserTypeId();
+        Users savedUser = usersRepository.save(users);
+        if(userTypeId==1){
+             recruiterProfileRepository.save(new RecruiterProfile(savedUser));
+        } else {
+            jobSeekerProfileRepository.save(new JobSeekerProfile(savedUser));
+        }
+        return savedUser;
     }
 
     public Optional<Users> getUserByEmail(String email){
