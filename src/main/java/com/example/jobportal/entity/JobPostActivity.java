5c3ca4004c26ee36dba8a7e7f6ce84b15fe76488
Mahmoud Gaber <71694567+MahmooudGaaber@users.noreply.github.com>
2025-07-22 15:01:39 +0300

    serach and apply flow

--- src/main/java/com/example/jobportal/entity/JobPostActivity.java
+++ src/main/java/com/example/jobportal/entity/JobPostActivity.java
@@ -92,19 +92,19 @@ public class JobPostActivity {
         this.jobCompanyId = jobCompanyId;
     }
 
-    public Boolean getActive() {
+    public Boolean getIsActive() {
         return isActive;
     }
 
-    public void setActive(Boolean active) {
+    public void setIsActive(Boolean active) {
         isActive = active;
     }
 
-    public Boolean getSaved() {
+    public Boolean getIsSaved() {
         return isSaved;
     }
 
-    public void setSaved(Boolean saved) {
+    public void setIsSaved(Boolean saved) {
         isSaved = saved;
     }
 
@@ -155,6 +155,7 @@ public class JobPostActivity {
     public void setJobTitle(String jobTitle) {
         this.jobTitle = jobTitle;
     }
+
 
     @Override
     public String toString() {
